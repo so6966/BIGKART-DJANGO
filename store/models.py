6a31ca2321a506a4311b9f7b568bdf5dfe1d7c5f
sohail <so6966@gmail.com>
2022-11-28 23:03:35 +0530

    store and carts functionality

--- store/models.py
+++ store/models.py
@@ -13,7 +13,7 @@ class Product(models.Model):
     stock         =models.IntegerField()
     is_available  =models.BooleanField(default=True)
     category      =models.ForeignKey(Category,on_delete=models.CASCADE)
-    Created_date  =models.DateTimeField(auto_now=True)
+    created_date  =models.DateTimeField(auto_now=True)
     modified_date =models.DateTimeField(auto_now_add=True)
 
 
@@ -22,3 +22,27 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
+
+class VariationManager(models.Manager):
+    def colors(self):
+        return super(VariationManager, self).filter(variation_category='color', is_active=True)
+
+    def sizes(self):
+        return super(VariationManager, self).filter(variation_category='size', is_active=True)
+
+variation_category_choice = (
+    ('color', 'color'),
+    ('size', 'size'),
+)
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=100, choices=variation_category_choice)
+    variation_value     = models.CharField(max_length=100)
+    is_active           = models.BooleanField(default=True)
+    created_date        = models.DateTimeField(auto_now=True)
+
+    objects = VariationManager()
+
+    def __str__(self):
+        return self.variation_value
