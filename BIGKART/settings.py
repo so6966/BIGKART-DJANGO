cc6b0ce53dd34414103aa1da56d5222d24de1837
sohail <so6966@gmail.com>
2022-12-08 22:27:20 +0530

    django authentication

--- BIGKART/settings.py
+++ BIGKART/settings.py
@@ -135,3 +135,18 @@ STATICFILES_DIRS=[
 # media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+
+#smtp configuration
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'so6966@gmail.com'
+EMAIL_HOST_PASSWORD = 'scmuzofwmbsuanpw'
+EMAIL_USE_TLS = True
